4a71cc2195c265fb2d86d7258e9ee2da92dd3aba
Martin <mselvenis@gmail.com>
2023-04-26 20:01:31 +0100

    checking environmental variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envVariables = z.object({
+  APCA_API_BASE_URL: z.string(),
+  APCA_API_KEY_ID: z.string(),
+  APCA_API_SECRET_KEY: z.string(),
+  OPENAI_API_KEY: z.string(),
+  DISCORD_TOKEN: z.string(),
+  DISCORD_CHANNEL_ID: z.string(),
+});
+
+envVariables.parse(process.env);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
+  }
+}
+
 console.log('Hello World!');
